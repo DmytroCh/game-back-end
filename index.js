8f3a174989e2e5801ef390e7524fac461a69a959
Dimabest92 <dimabest92@gmail.com>
2020-11-02 22:29:54 +0100

    route react app through node js server

--- index.js
+++ index.js
@@ -1,35 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
-// App setup
-const app = express();
 
 const hostname = '0.0.0.0';
 const port = 4000;
+const clientBuildPath = path.join(__dirname, '../game-front-end/build');
 
+// App setup
+const app = express();
 app.use(cors());
 
+// Serve static files from the React app
+app.use(express.static(clientBuildPath));
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', function(request, response) {
+  response.sendFile(path.join(clientBuildPath, 'index.html'));
+});
+
+// web sockets listener
 const server = app.listen(port, hostname, () => {
-    console.log(`Server running at ws://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
 
 const io = require('./model/socket').init(server);
 
 // Set listeners
 require('./listeners').setListeners(io);
-
-
-/*const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
