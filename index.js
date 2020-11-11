c273adfe06064a1848aa852e810e745f1aa6dcd0
DmytroCh <dimabest92@gmail.com>
2020-11-11 14:26:41 +0100

    Feature/dockerization (#4)
    * dockerization

--- index.js
+++ index.js
@@ -1,32 +1,19 @@
 const express = require('express');
+const cors = require('cors');
 
+const hostname = '0.0.0.0';
+const port = 3001;
 
 // App setup
 const app = express();
+app.use(cors());
 
-const hostname = '127.0.0.1';
-const port = 4000;
+// web sockets listener
 const server = app.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}`);
 });
 
 const io = require('./model/socket').init(server);
 
 // Set listeners
 require('./listeners').setListeners(io);
-
-
-/*const http = require('http');
-
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});*/
